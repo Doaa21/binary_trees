fa4a8bfc9a1a6bc3e61276b990d2c43a2cca8ea0
Doaa21 <doaaelhaweet2121@gmail.com>
2023-10-06 00:36:11 -0700

    binary_trees

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,113 +1,91 @@
 #include "binary_trees.h"
-/**
- * new_node - This creates a new node in a linked list
- * @node: Its a pointer type node to be created
- * Return: node created
- */
-link_t *new_node(binary_tree_t *node)
-{
-	link_t *new;
+#include <stdio.h>
 
-	new = malloc(sizeof(link_t));
-	if (new == NULL)
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of the tree. If tree is NULL, return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	binary_tree_t *l, *r;
+
+	if (tree == NULL)
+		return (1);
+	l = tree->left;
+	r = tree->right;
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	if (l == NULL || r == NULL)
+		return (0);
+	if (binary_tree_height(l) == binary_tree_height(r))
 	{
-		return (NULL);
+		if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
+			return (1);
 	}
-	new->node = node;
-	new->next = NULL;
-
-	return (new);
+	return (0);
 }
 /**
- * free_q - It frees a node at a linked list
- * @head: Node of linked list
- */
-void free_q(link_t *head)
-{
-	link_t *tmp_node;
-
-	while (head)
-	{
-		tmp_node = head->next;
-		free(head);
-		head = tmp_node;
-	}
-}
-/**
- * _push - It pushes a node into the stack
- * @node: type pointer of the node of the tree
- * @headhead node in the stack
- * @tail: tail node in the stack
- */
-void _push(binary_tree_t *node, link_t *head, link_t **tail)
-{
-	link_t *new;
-
-	new = new_node(node);
-	if (new == NULL)
-	{
-		free_q(head);
-		exit(1);
-	}
-	(*tail)->next = new;
-	*tail = new;
-}
-/**
- * _pop - It pos a node into the stack
- * @head: The head node in the stack
- */
-void _pop(link_t **head)
-{
-	link_t *tmp_node;
-
-	tmp_node = (*head)->next;
-	free(*head);
-	*head = tmp_node;
-}
-/**
- * binary_tree_is_complete - It checks if binary tree is complete
- * @tree: Pointer type of node tree
- * Return: 1 if complete, 0 is not
+ * binary_tree_is_complete -  checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if complete, 0 otherwise. If tree is NULL, return 0
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	link_t *head, *tail;
-	int flag = 0;
+	size_t l_height, r_height;
+	binary_tree_t *l, *r;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	head = tail = new_node((binary_tree_t *)tree);
-	if (head == NULL)
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	l = tree->left;
+	r = tree->right;
+	l_height = binary_tree_height(l);
+	r_height = binary_tree_height(r);
+	if (l_height == r_height)
 	{
-		exit(1);
+		if (binary_tree_is_perfect(l) && binary_tree_is_complete(r))
+			return (1);
 	}
-	while (head != NULL)
+	else if (l_height == r_height + 1)
 	{
-		if (head->node->left != NULL)
-		{
-			if (flag == 1)
-			{
-				free_q(head);
-				return (0);
-			}
-			_push(head->node->left, head, &tail);
-		}
-		else
-			flag = 1;
-		if (head->node->right != NULL)
-		{
-			if (flag == 1)
-			{
-				free_q(head);
-				return (0);
-			}
-			_push(head->node->right, head, &tail);
-		}
-		else
-			flag = 1;
-		_pop(&head);
+		if (binary_tree_is_complete(l) && binary_tree_is_perfect(r))
+			return (1);
 	}
-	return (1);
+	return (0);
 }
